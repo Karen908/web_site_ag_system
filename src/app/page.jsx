7e63344e5d87c81ad351b89da6347c4ada2e0ff6
Karen908 <131012640+Karen908@users.noreply.github.com>
2023-11-14 23:55:47 -0500

    Descarto elementos que, al final, determino que no utilizaré.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -63,22 +63,3 @@ export default function Home() {
   );
 }
 
-
-
-
-
-
-{/* <div class="hero-img">
-<img src='/img/Logos_Empresa/empresaImagotipo.png' alt="LogoAG" class="w-full h-auto object-cover" />
-</div> */}
-// <div>
-// <h1 className="text-black font-extrabold text-xl lg:text-4xl pl-28 pt-28 pb-2 md:pl-24 lg:pl-40">Bienvenido</h1>
-// <div className="min-h-screen text-white p-1">
-//   <div className="prose text-sm text-black max-w-2xl pl-24 md:pl-22 lg:pl-36 pr-20 lg:pr-0">
-//     <p >Somos una empresa en fase de construcción.</p>
-//     <p>Nos especializamos en el desarrollo de aplicaciones web y aplicaciones móviles. Seguimos los procedimientos y controles de nuestro sistema de calidad, buscando la eficacia para garantizar los resultados y satisfacer las necesidades del cliente.</p>
-//     <p>Actualmente estamos en la creación de CN_Mecab, producto en el que estamos trabajando y esperamos publicar muy pronto.</p>
-//     <p>SOMOS AG SYSTEM INFORMATION...</p>
-//   </div>
-// </div>
-// </div>
