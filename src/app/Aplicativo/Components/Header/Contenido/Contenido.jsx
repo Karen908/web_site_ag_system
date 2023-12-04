97150b0bab7c1499d504e5a2583ec352be4bbb92
Ivan Santiago Sepulveda <ivan.sepulveda3@soy.sena.edu.co>
2023-12-04 02:07:18 -0500

    correcion de ubicacion y dark mode prueba falta para toda la pagina

--- src/app/Aplicativo/Components/Header/Contenido/Contenido.jsx
+++ src/app/Aplicativo/Components/Header/Contenido/Contenido.jsx
@@ -2,14 +2,16 @@
 
 
 const Contenido = () => {
+
+    
     return (
-        <div className='w-full h-full md:w-1/2 lg:w-1/3 xl:w-1/4'>
+        <div>
         <div class="circle">
         </div>
 
-
-        
         </div>
+
+
 
 
 
--- src/app/Aplicativo/page.jsx
+++ src/app/Aplicativo/page.jsx
@@ -1,12 +1,13 @@
-import Header from "./Components/Header/Navbar";
+import Header from "./Components/Home/Navbar";
 import Contenido from "./Components/Header/Contenido/Contenido";
-
+import DarkMod from "./Components/Home/DarkMode";
 
 const page = () => {
     return (
         <div>
       <Header />
       <Contenido />
+
         </div>
     );
 };
