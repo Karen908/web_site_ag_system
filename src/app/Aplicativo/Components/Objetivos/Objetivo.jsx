25a288cb078daab9fd5151b493410a0fa1a0fa99
Joseph López Henao <joseph.lopez2@soy.sena.edu.co>
2023-12-11 20:45:13 -0500

    Correcion De Codigo Etiqueta "class"

--- src/app/Aplicativo/Components/Objetivos/Objetivo.jsx
+++ src/app/Aplicativo/Components/Objetivos/Objetivo.jsx
@@ -15,13 +15,13 @@ const Objetivo = () => {
         <Helmet>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Helmet>
-        <section class="objetivo section1" id="Objetivos">
-        <h1 class="heading font-bold"> <span>Objetivo</span> CN </h1>
-        <div class="row">
-            <div class="image">
-                <img class="pantalla" src="/img/Cn_mecab/Aplicacion/cnPantalla.png"/> 
+        <section className="objetivo section1" id="Objetivos">
+        <h1 className="heading font-bold"> <span>Objetivo</span> CN </h1>
+        <div className="row">
+            <div className="image">
+                <img className="pantalla" src="/img/Cn_mecab/Aplicacion/cnPantalla.png"/> 
             </div>
-            <div class="contento font-bold ">
+            <div className="contento font-bold ">
                 <h3 className="text-center">Nuestro Objetivo Eres Tú</h3>
                 <p>
                     Contribuir a los usuarios cinéfilos la exploración, 
@@ -35,8 +35,8 @@ const Objetivo = () => {
                          del cine.</p>
             </div>
         </div>
-        <a href="#" class="btn"><MdOutlineKeyboardDoubleArrowUp /></a>
-          <a href="#Que_Ofrecemos" class="btn_abajo"><MdOutlineKeyboardDoubleArrowDown /></a>
+        <a href="#" className="btn"><MdOutlineKeyboardDoubleArrowUp /></a>
+          <a href="#Que_Ofrecemos" className="btn_abajo"><MdOutlineKeyboardDoubleArrowDown /></a>
     </section>
     </>
 
